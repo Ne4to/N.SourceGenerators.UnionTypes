74ecb43e18bebaeab5fc8080937419be88be1898
Victor Irzak <victor.irzak@zomp.com>
2023-01-29 14:13:01 -0500

    feat: add benchmark to measure GetHash

--- src/N.SourceGenerators.UnionTypes.Benchmark/Program.cs
+++ src/N.SourceGenerators.UnionTypes.Benchmark/Program.cs
@@ -56,6 +56,45 @@ public class ReadValueBenchmark
         _classLayout.AsValidationError;
 }
 
+[MemoryDiagnoser]
+public class HashBenchmark
+{
+    private readonly FooStructResult _struct;
+    private readonly FooResult _class;
+    private readonly FooResultLayout _classLayout;
+    private readonly FooStructResultWithStructLayout _structLayout;
+    private readonly FooStructResultWithStructLayoutNoBoxing _structLayoutNoBoxing;
+
+    public HashBenchmark()
+    {
+        _struct = new FooStructResult(new ValidationErrorStruct(42));
+        _class = new FooResult(new ValidationError("something went wrong"));
+        _classLayout = new FooResultLayout(new ValidationError("something went wrong"));
+        _structLayout = new FooStructResultWithStructLayout(new ValidationErrorStruct(42));
+        _structLayoutNoBoxing = new FooStructResultWithStructLayoutNoBoxing(new ValidationErrorStruct(42));
+    }
+
+    [Benchmark]
+    public int Struct() =>
+        _struct.GetHashCode();
+
+    [Benchmark]
+    public int WithLayout() =>
+        _structLayout.GetHashCode();
+
+    [Benchmark]
+    public int WithLayoutNoBoxing() =>
+        _structLayoutNoBoxing.GetHashCode();
+
+    [Benchmark(Baseline = true)]
+    public int Class() =>
+        _class.GetHashCode();
+
+    [Benchmark]
+    public int ClassLayout() =>
+        _classLayout.GetHashCode();
+}
+
 public record struct SuccessStruct(int Value);
 
 public record struct ValidationErrorStruct(int ErrorCode);
